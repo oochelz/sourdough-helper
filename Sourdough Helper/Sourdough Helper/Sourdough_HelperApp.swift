a4ee86e9584f1153f3f96b5983ef2fc3a9da6cce
Chelsea Youmans <chelsea@Chriss-MacBook-Pro.local>
2021-05-28 21:47:44 -0400

    Initial Commit

---
+++ Sourdough Helper/Sourdough Helper/Sourdough_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sourdough_HelperApp.swift
+//  Sourdough Helper
+//
+//  Created by Chelsea Youmans on 5/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Sourdough_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
