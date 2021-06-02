9911cb637a6cc70ae845455e5ead537d90f22240
Chelsea Youmans <chelsea@oochelz.com>
2021-06-02 08:39:27 -0400

    Allow input

--- Sourdough Helper/Sourdough Helper/ContentView.swift
+++ Sourdough Helper/Sourdough Helper/ContentView.swift
@@ -11,16 +11,16 @@ import UserNotifications
 struct ContentView: View {
     @State private var showingAlert = false
     @State private var showingSuccess = false
+    @State private var intervalInMin = 5.0
+    @State private var durationInHr = 1.0
     
     fileprivate func scheduleNotifications() {
         let content = UNMutableNotificationContent()
         content.title = "Stretch the dough"
         content.body = "It's that time. Stretchy stretchy."
         content.sound = UNNotificationSound.default
-        let intervalInMin = 30
-        let durationInHr = 4
-        let intervalInSec = intervalInMin * 60
-        let numNotifications = durationInHr * 60 / intervalInMin
+        let intervalInSec = Int(intervalInMin) * 60
+        let numNotifications = Int(durationInHr) * 60 / Int(intervalInMin)
         for index in 1...numNotifications {
             let trigger = UNTimeIntervalNotificationTrigger(timeInterval: Double(intervalInSec * index), repeats: false)
             let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
@@ -31,14 +31,38 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
+            Form {
+                Section {
+                    Text("How often do you need to fold?")
+                    HStack {
+                        Image(systemName: "minus")
+                        Slider(value: $intervalInMin, in: 5...60, step: 5)
+                        Image(systemName: "plus")
+                    }
+                    Text("Every \(Int(intervalInMin)) minutes")
+                        .font(.subheadline)
+                }
+                Section {
+                    Text("For how long?")
+                    HStack {
+                        Image(systemName: "minus")
+                        Slider(value: $durationInHr, in: 1...12, step: 1)
+                        Image(systemName: "plus")
+                    }
+                    Text("\(Int(durationInHr)) hours")
+                        .font(.subheadline)
+                }
                 Button("Start Timer") {
                     let center = UNUserNotificationCenter.current()
-                    center.requestAuthorization(options: [.alert, .badge, .sound, .provisional]) { success, error in }
+                    center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                        if error != nil {
+                            showingAlert = true
+                            return
+                        }
+                    }
                     
                     center.getNotificationSettings { settings in
-                        guard (settings.authorizationStatus == .authorized) ||
-                              (settings.authorizationStatus == .provisional) else {
+                        guard (settings.authorizationStatus == .authorized) else {
                             showingAlert = true
                             return
                         }
@@ -59,9 +83,6 @@ struct ContentView: View {
                         dismissButton: .default(Text("OK"))
                     )
                 })
-                .padding()
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .foregroundColor(.green)
             }
             .navigationTitle("Sourdough Helper")
         }
@@ -70,6 +91,8 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ForEach(ColorScheme.allCases, id: \.self) {
+             ContentView().preferredColorScheme($0)
+        }
     }
 }
